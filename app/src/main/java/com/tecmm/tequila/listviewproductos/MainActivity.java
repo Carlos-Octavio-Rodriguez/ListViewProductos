c2dd396c253ce2d5bffd4c3a18b91ac8f6f98f1d
Carlos Octavio R <carlos.octavio.r1997@gmail.com>
2018-04-20 13:03:45 -0500

    Actualizado a actividad 9

--- app/src/main/java/com/tecmm/tequila/listviewproductos/MainActivity.java
+++ app/src/main/java/com/tecmm/tequila/listviewproductos/MainActivity.java
@@ -1,5 +1,6 @@
 package com.tecmm.tequila.listviewproductos;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -11,14 +12,18 @@ import android.widget.TextView;
 import org.w3c.dom.Text;
 
 import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
     TextView txt;
     ListView lista;
 
-    private String productos[] = {"Computadora", "Mouse", "Dulces", "Lapices", "Hojas", "Horno", "Televisión", "Reloj", "Cucharas"};
-    private String categoria[] = {"Electronica", "Electronica", "Dulceria", "Papeleria", "Papeleria", "Cocina", "Electronica", "Moda", "Cocina"};
+    ///private String productos[] = {"Computadora", "Mouse", "Dulces", "Lapices", "Hojas", "Horno", "Televisión", "Reloj", "Cucharas"};
+    ///private String categoria[] = {"Electronica", "Electronica", "Dulceria", "Papeleria", "Papeleria", "Cocina", "Electronica", "Moda", "Cocina"};
+    private List<String> lProductos = new ArrayList<>();
+    private List<String> lcategoria = new ArrayList<>();
 
 
     @Override
@@ -28,7 +33,9 @@ public class MainActivity extends AppCompatActivity {
         txt = (TextView) findViewById(R.id.txt);
         lista = (ListView) findViewById(R.id.lista);
 
-        ArrayAdapter<String>adapter = new ArrayAdapter<String>(this, R.layout.support_simple_spinner_dropdown_item ,productos);
+
+
+      /* ArrayAdapter<String>adapter = new ArrayAdapter<String>(this, R.layout.support_simple_spinner_dropdown_item ,productos);
         lista.setAdapter(adapter);
 
         lista.setOnItemClickListener(new AdapterView.OnItemClickListener() {
@@ -36,7 +43,35 @@ public class MainActivity extends AppCompatActivity {
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 txt.setText("Categoria elegida: " + categoria[position]);
             }
-        });
+        });*/
 
+    }
+
+    private void actualizarTabla(){
+        String productos[] = new String[lProductos.size()];
+        lProductos.toArray(productos);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, R.layout.support_simple_spinner_dropdown_item, productos);
+        lista.setAdapter(adapter);
+
+        lista.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                txt.setText("Categoria elegida: " + lcategoria.get(position));
+            }
+        });
+    }
+
+    ///boton
+    public void llamarActividad(View x){
+        Intent i = new Intent(this, actividad.class);
+        startActivityForResult(i, 123);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        lProductos.add(data.getStringExtra("nombre"));
+        lcategoria.add(data.getStringExtra("categoria"));
+        actualizarTabla();
     }
 }
---
+++ app/src/main/java/com/tecmm/tequila/listviewproductos/actividad.java
@@ -0,0 +1,52 @@
+package com.tecmm.tequila.listviewproductos;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class actividad extends AppCompatActivity {
+
+    Spinner spinner;
+    EditText txtnombre;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_actividad);
+        String categorias[] = {"Electronica", "Hogar", "Papeleria", "Moda"};
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, R.layout.support_simple_spinner_dropdown_item, categorias);
+
+        spinner = (Spinner) findViewById(R.id.spnProducto);
+        spinner.setAdapter(adapter);
+        txtnombre = (EditText) findViewById(R.id.edTxtNombre);
+    }
+
+
+
+    public void agregar(View v){
+        Intent i = new Intent();
+        i.putExtra("nombre", txtnombre.getText().toString());
+        i.putExtra("categoria", spinner.getSelectedItem().toString());
+        setResult(RESULT_OK,i);
+
+        LayoutInflater inflater = getLayoutInflater();
+        View layout = inflater.inflate(R.layout.toast_layout, null);
+
+        TextView text = (TextView) layout.findViewById(R.id.txt);
+        text.setText("Se ha agregado " + txtnombre.getText() + " correctamente");
+
+        Toast toast = new Toast(getApplicationContext());
+        toast.setDuration(Toast.LENGTH_LONG);
+        toast.setView(layout);
+        toast.show();
+
+        finish();
+    }
+}
